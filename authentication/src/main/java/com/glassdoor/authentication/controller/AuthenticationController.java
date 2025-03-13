28fc9a0c37fc981ebcc0024ae5a701bc0d61e5df
Mariam1341 <mmmm01061480913@gmail.com>
2025-03-13 15:52:12 +0200

    register and authenticate

---
+++ authentication/src/main/java/com/glassdoor/authentication/controller/AuthenticationController.java
@@ -0,0 +1,30 @@
+package com.glassdoor.authentication.controller;
+
+import com.glassdoor.authentication.dto.AuthenticationRequest;
+import com.glassdoor.authentication.dto.AuthenticationResponse;
+import com.glassdoor.authentication.dto.RegisterRequest;
+import com.glassdoor.authentication.service.AuthenticationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request){
+        return ResponseEntity.ok(authenticationService.register(request));
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request){
+        return ResponseEntity.ok(authenticationService.authenticate(request));
+    }
+}
