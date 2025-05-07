e54cc39879e7f5aa8efb9b4b69bf812e1cf64606
abdelrahmanfaheem <abdofaheem4@gmail.com>
2025-05-07 11:56:08 +0300

    .

--- Backend/src/main/java/com/glassdoor/backend/controller/CompanyController.java
+++ Backend/src/main/java/com/glassdoor/backend/controller/CompanyController.java
@@ -50,4 +50,3 @@ public class CompanyController {
         return companyService.searchByName(name);
     }
 }
-
--- Backend/src/main/java/com/glassdoor/backend/controller/JobController.java
+++ Backend/src/main/java/com/glassdoor/backend/controller/JobController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1/job")
 @RequiredArgsConstructor
 @CrossOrigin(origins = "http://localhost:3000")
-
 public class JobController {
 
     @Autowired
